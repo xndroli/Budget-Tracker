e30edf23ddebc5a8aa0dfa25e4106f099e1a032d
xndroli <iamsimplyclean@knights.ucf.edu>
2022-08-27 16:53:08 -0400

    added service worker to app

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,9 @@
+// global constants
 const APP_PREFIX = "Budget-Tracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
+// files to be cached by prioirity
 const FILES_TO_CACHE = [
   "./index.html",
   "./public/js/index.js",
@@ -18,6 +20,7 @@ const FILES_TO_CACHE = [
   "./icons/icon-512x512.png",
 ];
 
+// install service worker and add files to the precache, so that the application can use the cache
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -25,4 +28,48 @@ self.addEventListener("install", function (e) {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+});
+
+// clear out any old data from the cache and, in the same step, tell the service worker how to manage caches
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add the current cache to the keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      // returns a promise that resolves once all old versions of the cache have been deleted
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+// intercept fetch requests
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith();
+});
+
+// determine if the resource already exists in caches
+caches.match(e.request).then(function (request) {
+  // If it does, log the URL to the console with a message and then return the cached resource
+  if (request) {
+    console.log("responding with cache : " + e.request.url);
+    return request;
+  }
+});
+
+// determine if the resource is not already exists in caches
+caches.match(e.request).then(function (request) {
+  // If it does not, allow the resource to be retrieved from the online network as usual
+  return request || fetch(e.request);
 });

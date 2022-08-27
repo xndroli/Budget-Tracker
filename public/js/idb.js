c47a695767a158143e65f4e350c9cb8252931e7d
xndroli <iamsimplyclean@knights.ucf.edu>
2022-08-27 15:21:30 -0400

    added upload transaction data logic

--- public/js/idb.js
+++ public/js/idb.js
@@ -18,7 +18,7 @@ request.onsuccess = function (event) {
 
   // check if app is online, if yes run uploadTransaction() function to send all local db data to api
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
+    // haven't created this yet, but I will soon..
     // insert uploadTransaction();
   }
 };
@@ -39,3 +39,49 @@ function saveRecord(record) {
   // add record to your store with add method
   budgetObjectStore.add(record);
 }
+
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  // access your object store
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          // access the new_transaction object store
+          const budgetObjectStore = transaction.objectStore("new_transaction");
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert("All saved transactions have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", uploadTransaction);
